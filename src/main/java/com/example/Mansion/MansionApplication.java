29c1e3a3e94df2a3d399f203d37df87e4f846382
kkobrok <kkobrok@gmail.com>
2020-11-01 02:13:46 +0100

    First commit

---
+++ src/main/java/com/example/Mansion/MansionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mansion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MansionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MansionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mansion/MansionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mansion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MansionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
